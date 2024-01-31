5995273f08f53457dd5882a23a7134e4cd3097cf
fjl005 <frank.jk.lee96@gmail.com>
2024-01-30 19:56:13 -0800

    removed port to test it

--- server.js
+++ server.js
@@ -157,8 +157,8 @@ const connect = async () => {
 }
 connect();
 
-const port = 5000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// const port = 5000;
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
 
