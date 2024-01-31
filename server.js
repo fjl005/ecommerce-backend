ee6c936499184ba513c786a361007204b3418164
fjl005 <frank.jk.lee96@gmail.com>
2024-01-30 21:07:20 -0800

    irrelevant

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://example.com'); // Replace with your frontend origin
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add any additional headers your frontend uses
     next();
