766b5ef0c13d90b46a2f862279883f4df50cd158
fjl005 <frank.jk.lee96@gmail.com>
2024-02-05 21:09:43 -0800

    extended session to one day (instead of 1 hour)

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use(session({
     saveUninitialized: false,
     store: store,
     cookie: {
-        maxAge: 1000 * 60 * 60,
+        maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
         // secure: true,
         // sameSite: 'none',
